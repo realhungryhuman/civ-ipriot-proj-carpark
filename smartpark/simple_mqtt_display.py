9c97796b5d5dc477bce83197d30fac7ec1d9c1e1
Dylan McClarence <20111795@tafe.wa.edu.au>
2023-11-15 18:55:53 +0800

    Implemented SenseHat Display

--- smartpark/simple_mqtt_display.py
+++ smartpark/simple_mqtt_display.py
@@ -2,6 +2,7 @@ import mqtt_device
 import time
 from config_parser import parse_config
 from sys import argv
+from sense_emu import SenseHat
 
 DEVICE_NUMBER = int(argv[1]) - 1
 
@@ -12,6 +13,7 @@ class Display(mqtt_device.MqttDevice):
         super().__init__(config)
         self.client.on_message = self.on_message
         self.client.subscribe('lot/moondalup/display1/na')
+        self.sense_hat = SenseHat()
         self.client.loop_forever()
 
     def display(self, *args):
@@ -19,15 +21,16 @@ class Display(mqtt_device.MqttDevice):
         for val in args:
             print(val)
             time.sleep(1)
-
         print('*' * 20)
+
+        for val in args:
+            self.sense_hat.show_message(val)
 
     def on_message(self, client, userdata, msg):
        data = msg.payload.decode()
        self.display(*data.split(','))
        # TODO: Parse the message and extract free spaces,\
        #  temperature, time
-       # Already done(?)
 
 
 if __name__ == '__main__':
